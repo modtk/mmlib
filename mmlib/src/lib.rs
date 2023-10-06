8eac936e9ba76ec033abcf907c946b2b447f8161
rosidae <82954131+rosidae@users.noreply.github.com>
2023-10-05 19:05:49 -0500

    welcome

--- mmlib/src/lib.rs
+++ mmlib/src/lib.rs
@@ -1,4 +1,6 @@
-/// There is a formats module, go to the documentation for that module, and look for desired file types.
+//! # Welcome to the mmlib documentation.
+//! There is a formats module, go to the documentation for that module, and look for desired file types.
+
 #[macro_export]
 /// This macro is used to quickly re-export modules through a list.
 macro_rules! quickmodule {
